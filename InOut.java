b2d1f688b4026ccf585583737e4985e129c974a5
Reshma V <reshmav912@gmail.com>
2018-07-10 21:57:26 +0530

    Add files via upload

---
+++ InOut.java
@@ -0,0 +1,22 @@
+package sample;
+
+import java.util.Scanner;
+import java.io.IOException;
+
+
+public class InOut {
+
+  public static void main(String[] args) throws IOException {
+
+      @SuppressWarnings("resource")
+	  Scanner scan = new Scanner(System.in);
+      int i = scan.nextInt();
+      double d = scan.nextDouble();
+      scan.nextLine();
+      String s = scan.nextLine();
+      System.out.println("String: " + s);
+      System.out.println("Double: " + d);
+      System.out.println("Int: " + i);
+  }
+  
+}
