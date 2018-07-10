e1e9750608eea23f66a08252d307e40022f3d92a
Reshma V <reshmav912@gmail.com>
2018-07-10 16:13:49 +0530

    Update PlayWithDice.java

--- PlayWithDice.java
+++ PlayWithDice.java
@@ -1,5 +1,6 @@
 package sample;
-
+/*program to obtain a random number when a dice is rolled
+  autor Reshma*/
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,8 +8,8 @@ import java.io.InputStreamReader;
 public class PlayWithDice {
 	public double randomGenerate(int sides){
 		double randomNo = Math.random();
-		randomNo *= sides;
-		randomNo += 1;
+		randomNo *= sides;                    //the no of sides of the dice
+		randomNo += 1;                       //Generated random number would include 0, to avoid this 1 is added
 		return randomNo;
 	}
 	
@@ -16,9 +17,9 @@ public class PlayWithDice {
 		PlayWithDice obj = new PlayWithDice();
 		InputStreamReader istream = new InputStreamReader(System.in);
 		BufferedReader bufRead = new BufferedReader(istream);
-		System.out.println("Enter a number\n");
+		System.out.println("Enter the number of sides of the dice\n");
 		String dice = bufRead.readLine();
-		int diceSide = Integer.parseInt(dice);
+		int diceSide = Integer.parseInt(dice);              //the input is a string. it is parsed to an integer
 		double generateNo = obj.randomGenerate(diceSide);
 		int generate = (int)generateNo;
 		System.out.println(generate);
